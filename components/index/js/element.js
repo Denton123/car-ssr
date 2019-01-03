6f1933f3ea9b0f82271bf65900cf010bc8ed6d38
e <zhengbangzong@do1.com.cn>
2019-01-03 11:29:13 +0800

    移除旧页面

--- components/index/js/element.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import { Carousel, CarouselItem } from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-
-export const element = () => {
-  Vue.use(Carousel);
-  Vue.use(CarouselItem);
-};
