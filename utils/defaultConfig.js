16f70b9d7d776e255413cad39051240897261d4a
zgbbiao <1031516418@qq.com>
2019-03-01 16:38:22 +0800

    xiuga

--- utils/defaultConfig.js
+++ utils/defaultConfig.js
@@ -1,8 +1,8 @@
 
 //切换不同环境的接口
 
-// export const pc_URL = 'http://www.jfcar.com.cn'
-export const pc_URL = 'http://165.qiweioa.cn'
+export const pc_URL = 'http://www.jfcar.com.cn'
+// export const pc_URL = 'http://165.qiweioa.cn'
 
 export const detailsObj = {
   hots: 'model',
